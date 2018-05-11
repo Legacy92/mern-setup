5659eed8eda62c0385a826bc4763f1ce9d07ab21
Legacy92 <colemancorey22@gmail.com>
2018-05-10 17:45:34 -0700

    MERN setup for deployment completed

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -5,10 +5,31 @@ class Test extends Component {
 
     componentDidMount(){
         this.getUserData();
+        this.getArticle();
+        this.sendData();
     }
 
+async sendData(){
+    const dataToSend = {
+        something: 'Here is something',
+        anotherThing: 'Here is something else',
+        stuff: 'Well why not?'
+    }
+    
+    const response = await axios.post('/api/send-data', dataToSend);
+    
+    console.log('Send Data:', response);
+}
+
+async getArticle(){
+    const response = await axios.get('/api/get-article');
+
+    console.log('Article Data:', response);
+}
+
 async getUserData() {
-    const response = await axios.get('http://localhost:9000/user-data');
+    const response = await axios.get('/api/user-data');
+
 
     console.log('Useer Data Response:', response);
 }
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { resolve } = require('path');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data', (req, res) => {
+app.post('/api/send-data', (req, res) => {
+    console.log('Data sent:', req.body);
+
+    res.send({success: true, mirror: req.body});
+});
+
+app.get('/api/user-data', (req, res) => {
     const user = {
         name: 'Jim Bob',
         email: 'jimthebob@mail.com'
@@ -14,8 +23,18 @@ app.get('/user-data', (req, res) => {
     res.send(user);
 });
 
-app.get('/', (req,res) => {
-    res.send('<h1>Completely Different</h1>');
+app.get('/api/get-article', (req, res) => {
+    const article = {
+        title: 'Something awesome that it is an Artcle',
+        author: 'Some dude',
+        content: 'once upon a time...in MEXICO'
+    }
+    
+    res.send(article);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
